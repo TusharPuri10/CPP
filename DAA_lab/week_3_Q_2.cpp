8ed761457f182df899f9275c78dee9ce0d6ea576
Tushar <iamtusharpuri@gmail.com>
2023-04-10 06:16:11 +0530

    practice

--- DAA_lab/week_3_Q_2.cpp
+++ DAA_lab/week_3_Q_2.cpp
@@ -1,6 +1,6 @@
-// Given an unsorted array of integers, design an algorithm and a program to sort the array using
-// insertion sort. Your program should be able to find number of comparisons and shifts ( shifts -
-// total number of times the array elements are shifted from their place) required for sorting the array
+// Given an unsorted array of integers, design an algorithm and implement a program to sort this
+// array using selection sort. Your program should also find number of comparisons and number of
+// swaps required.
 
 #include<iostream>
 using namespace std;
